7a3f0128ec8cbba813f3a3634656ef86f5c26e95
Tim <141640156+TStokes06@users.noreply.github.com>
2024-01-24 03:51:28 +0000

    NavbarNOTREADY

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext"
 import { Link, useNavigate } from "react-router-dom";
+import Logo from "../../img/Logo.png";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
   const { store, actions } = useContext(Context);
@@ -12,26 +14,42 @@ export const Navbar = () => {
   };
 
   return (
-    <nav className="navbar navbar-light bg-light">
-      <div className="container">
-        <Link to="/">
-          <span className="navbar-brand mb-0 h1" style={{ display: 'flex', alignItems: 'center' }}>TeamUp Nexus</span>
-        </Link>
-        <div className="ml-auto">
-          {!store.token ? (
-            <div>
-              <Link to="/signup">
-                <button className="btn btn-success signup">Sign up</button>
-              </Link>
-              <Link to="/login">
-                <button className="btn btn-primary">Log in</button>
-              </Link>
-            </div>
-          ) : (
-            <button onClick={handleLogout} className="btn btn-primary">Logout</button>
-          )}
+    <nav className="navbar-expand-md navbar" style={{ color: "#ffcc66", backgroundColor: "transparent", borderColor: "#ffcc66" }}>
+      <Link to="/home">
+        <span className="navbar-brand mb-0 h1" style={{ color: "#fff" }}><img src={Logo} height={150} width={225}/></span>
+      </Link>
+      {/* <div className="navbar justify-left"> */}
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="btnnavbar">
+          <Link to="/home" className="btnnavbar, nav-link" style={{ color: "#ffcc66", backgroundColor: "transparent", borderColor: "#ffcc66" }}>Home</Link>
+        </li>
+        <li className="btnnavbar">
+          <Link to="/profile" className="nav-link" style={{ color: "#ffcc66", backgroundColor: "transparent", borderColor: "#ffcc66" }}>Profile</Link>
+        </li>
+        <li className="btnnavbar">
+          <Link to="/messages" className="nav-link" style={{ color: "#ffcc66", backgroundColor: "transparent", borderColor: "#ffcc66" }}>Messages</Link>
+        </li>
+        <li className="btnnavbar">
+          <Link to="/search" className="nav-link" style={{ color: "#ffcc66", backgroundColor: "transparent", borderColor: "#ffcc66" }}>Search</Link>
+        </li>
+        <li className="btnnavbar">
+          <Link to="/settings" className="nav-link" style={{ color: "#ffcc66", backgroundColor: "transparent", borderColor: "#ffcc66" }}>Settings</Link>
+        </li>
+      </ul>
+      {/* </div> */}
+      {!store.token ? (
+        <div>
+          <Link to="/signup">
+            <button className="btnnavbar" style={{ color: "#ffcc66", backgroundColor: "transparent", borderColor: "#ffcc66" }}>Sign up</button>
+          </Link>
+          <Link to="/login">
+            <button className="btnnavbar" style={{ color: "#ffcc66", backgroundColor: "transparent", borderColor: "#ffcc66" }}>Log in</button>
+          </Link>
+
         </div>
-      </div>
+      ) : (
+        <button onClick={handleLogout} className="btn btn-" style={{ color: "#ffcc66" }}>Logout</button>
+      )}
     </nav>
   );
 };
