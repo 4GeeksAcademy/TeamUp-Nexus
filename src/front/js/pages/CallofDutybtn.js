5052c627b601ae477fa038b9246130d48b0eb5d9
Philippe <82619578+phildesignsgfx@users.noreply.github.com>
2024-01-28 23:29:43 +0000

    Almostdone

--- src/front/js/pages/CallofDutybtn.js
+++ src/front/js/pages/CallofDutybtn.js
@@ -1,11 +1,31 @@
 import React, { useContext, useEffect } from "react";
 import "../../styles/CallofDutybtn.css";
+import gamesmx1 from "../../img/gamesm1.png";
+import gamesmx2 from "../../img/gamesm2.png";
+import gamesmx3 from "../../img/gamesm3.png";
+import gamesmx4 from "../../img/gamesm4.png";
+import kingtut from "../../img/kingtut.png";
+import massroxx from "../../img/MassRoxx.png";
+import phil from "../../img/batman.png";
 
 export const Cod = () => {
 
 	return (
-		<div className="text-center mt-5 name=phil">
-			<h1>HIIIII</h1>
+
+
+		<div className>
+
+			hiiii
 		</div>
+
+
+		
+
+
+
+
+
+
+
 	);
 };
