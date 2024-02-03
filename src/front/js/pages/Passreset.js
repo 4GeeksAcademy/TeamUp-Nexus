5b8c478750db8d8ea8b6d8dca9ac5e043e1f3123
Philippe <82619578+phildesignsgfx@users.noreply.github.com>
2024-02-03 02:33:58 +0000

    almostdoneboys

--- src/front/js/pages/Passreset.js
+++ src/front/js/pages/Passreset.js
@@ -1,7 +1,9 @@
 import React, { useContext,useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
+import Logo from "../../img/Logo.png";
 import "../../styles/home.css";
+import "../../styles/Passreset.css";
 
 export const ResetPassword = () => {
   const { store,actions } = useContext(Context);
@@ -24,11 +26,29 @@ export const ResetPassword = () => {
   };
 
   return (
-    <div>
+    <div className="d-flex justify-content-center align-items-center bg-dark-purple vh-100">
+    <div className=" bg-secondary rounded w-25 text-dark ">
+      <img src={Logo} height={125} width={280} ClassName="logo22" />
+
+      <p>
+					<a href="" class="btn btn-block btn-twitter"> <i class="fab fa-twitter"></i>   Login via Twitter</a>
+					<br />
+					<a href="" class="btn btn-block btn-facebook"> <i class="fab fa-facebook-f"></i>   Login via facebook</a>
+				</p>
+				<p class="divider-text">
+					<span class="bg-light">OR</span>
+				</p>
+      
       <h2>Reset Password</h2>
       <h2>Enter Email</h2>
-      <input type="text" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <button onClick={handleResetPassword}>Reset Password</button>
+      <div className="input-container mb-3 text-start mx-1 mx-md-4 form-control-sm">
+      <input type="text" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)}  />
+      </div>
+      <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+      <button onClick={handleResetPassword} className="btn btn-dark w-100" >Reset Password</button>
+      </div>
     </div>
+    </div>
+    
   );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,7 +4,20 @@ import rigoImageUrl from "../../img/logomain2.png";
 import rigoImageUrl2 from "../../img/TeamupnexusCallofduty.png";
 import rigoImageUrl3 from "../../img/TeamUpNexusDeadspace.png";
 import rigoImageUrl4 from "../../img/TeamUpNexusFortnite.png";
+import twoset1 from "../../img/apexl1.png";
+import twoset2 from "../../img/bbf1.png";
+import twoset3 from "../../img/eft1.png";
+import apple from "../../img/appstorew.png";
+import google from "../../img/googlew.png";
+import idea from "../../img/idea.png";
+import rocket from "../../img/rocket-ship.png";
+import controller from "../../img/controller.png";
+import monitor from "../../img/monitor.png";
+import lbullets from "../../img/Leftullets.png";
+import rbullets from "../../img/leftside.png";
+
 import "../../styles/home.css";
+import "../../styles/style.scss";
 import { Link, useNavigate } from "react-router-dom";
 
 export const Home = () => {
@@ -27,74 +40,201 @@ export const Home = () => {
 
 		<div class="container">
 
-			<div class="row justify-content-start">
+		<div class="row justify-content-start">
 
-				<div class="background-container">
+			<div class="background-container">
 
+				<div class="hero-image">
 
+					<div class="hero-text">
 
-					<div class="hero-image">
+						{/* <h1> Forge Friendships,<h2>Conquer Challenges, and Level Up Together!</h2></h1> */}
 
-
-						<div class="hero-text">
-
-
-							{/* <h1> Forge Friendships,<h2>Conquer Challenges, and Level Up Together!</h2></h1> */}
-
-							<p className="mainTitle">
-							   Where Gamers Unite: Forge Friendships
-								<span>
-								Conquer Challenges
-								</span>
-								and Level Up Together!
-							</p>
-							{<img src={rigoImageUrl} />}
+						<p className="mainTitle">
+						   Where Gamers Unite: Forge Friendships
+							<span>
+							Conquer Challenges
+							</span>
+							and Level Up Together!
+						</p>
+						{<img src={rigoImageUrl} />}
+						<div>
+						{<img src={lbullets} height={320} width= {900} className="lbullets"/>}
+						{<img src={rbullets} height={320} width= {900} className="rside" />}
+						</div>
+						<br/>
+						<br/>
+						
+						<div>
+						<h1>Available on the following platforms</h1>
 							<br/>
-							<br/>
-							<br/>
+						</div>
+						<div class="d-flex justify-content-center">
+							
+						{<img src={apple}  height={80} width= {225}/>}
+						{<img src={google}  height={80} width= {225} />}
+
+
+						</div>
+						<br/>
+						<hr class="rounded"></hr>
+						<br/>
+
+
+						{/* <!-- do section --> */}
+  <section class="do_section layout_padding">
+    <div class="container">
+      <div class="custom_heading-container">
+      
+      </div>
+      <div class="row">
+        <div class="col-md-3 col-sm-6">
+          <div class="content-box bg-red">
+            <div class="img-box">
+			{<img src={idea} />}
+            </div>
+            <div class="detail-box">
+              <h6>
+                Original Ideas
+              </h6>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the readable content
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <div class="content-box bg-green">
+            <div class="img-box">
+			{<img src={rocket} />}
+            </div>
+            <div class="detail-box">
+              <h6>
+                Great apps
+              </h6>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the readable content
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <div class="content-box bg-red">
+            <div class="img-box">
+			{<img src={monitor} />}
+            </div>
+            <div class="detail-box">
+              <h6>
+                High Resolution
+              </h6>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the readable content
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <div class="content-box bg-green">
+            <div class="img-box">
+			{<img src={controller} />}
+            </div>
+            <div class="detail-box">
+              <h6>
+                Fast Loading
+              </h6>
+              <p>
+                It is a long established fact that a reader will be distracted
+                by the readable content
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  {/* <!-- end do section --> */}
 
 
 
 
 
-							<div class="col-lg-4 col-md-4 col-sm-4 container justify-content-center">
-								{/* <div class="row "> */}
-								<div class="col-10">
-									<div class="card custom-card-class">
+						<br/>
 
-										{<img src={rigoImageUrl3} alt className = "cod1" />} 
-									</div>
+
+						<div class="col-lg-4 col-md-4 col-sm-4 container justify-content-center">
+							{/* <div class="row "> */}
+							<div class="col-10">
+								<div class="card custom-card-class">
+
+									{<img src={rigoImageUrl3}  />} 
 								</div>
-								<div class="col-10">
-									<div class="card custom-card-class">
+							</div>
+							<div class="col-10">
+								<div class="card custom-card-class">
 
-										{<img src={rigoImageUrl2} />}
-									</div>
+									{<img src={rigoImageUrl2} />}
 								</div>
-								<div class="col">
-									<div class="card custom-card-class">
+							</div>
+							<div class="col">
+								<div class="card custom-card-class">
 
-										{<img src={rigoImageUrl4} />}
+									{<img src={rigoImageUrl4} />}
 
-										<div class="stars"></div>
-
-										<div class="container ">
-
-										</div>
+									
 
 									</div>
-
 
 								</div>
 
 							</div>
-							<br />
-							<br />
+
+							<br/>
+
+
+							<div class="col-lg-4 col-md-4 col-sm-4 container justify-content-center">
+							{/* <div class="row "> */}
+							<div class="col-10">
+								<div class="card custom-card-class">
+
+									{<img src={twoset1}  />} 
+								</div>
+							</div>
+							<div class="col-10">
+								<div class="card custom-card-class">
+
+									{<img src={twoset2} />}
+								</div>
+							</div>
+							<div class="col">
+								<div class="card custom-card-class">
+
+									{<img src={twoset3} />}
+
+									
+
+									</div>
+
+								</div>
+
+
+							</div>
+
 						</div>
+						<br />
+						<br />
+						
 					</div>
 				</div>
+				
 			</div>
+			
 		</div>
+		
+	
 
-	);
+);
 };
